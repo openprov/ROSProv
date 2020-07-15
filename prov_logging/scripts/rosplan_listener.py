2901cc79e1d8e8b7b412f8e79f3b7fbe9702e964
Trung Dong Huynh <trungdong@donggiang.com>
2020-07-15 15:05:39 +0100

    Added more message types to track

--- prov_logging/scripts/rosplan_listener.py
+++ prov_logging/scripts/rosplan_listener.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
+from pprint import pprint
 
 import rospy
-from rosplan_dispatch_msgs.msg import EsterelPlan
+from rosplan_dispatch_msgs.msg import EsterelPlan, ActionDispatch, ActionFeedback
+from rosplan_knowledge_msgs.msg import StatusUpdate
 
-def callback(data):
+def callback(data, data_class):
     rospy.loginfo(rospy.get_caller_id() + ': %s', data)
+    pprint("Data class: " + str(data_class))
+    pprint(data._connection_header)
 
-def listener():
+ROSPLAN_TOPICS = {
+    "/rosplan_knowledge_base/status/update": StatusUpdate,
+    "/rosplan_parsing_interface/complete_plan": EsterelPlan,
+    "/rosplan_plan_dispatcher/action_dispatch": ActionDispatch,
+    "/rosplan_plan_dispatcher/action_feedback": ActionFeedback,
+}
+
+
+def main():
 
     rospy.init_node('rosplan_listener', anonymous=False)
 
-    rospy.Subscriber('/rosplan_parsing_interface/complete_plan', EsterelPlan, callback)
+    for topic, data_class in ROSPLAN_TOPICS.items():
+        rospy.Subscriber(topic, data_class, callback, data_class)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    main()
